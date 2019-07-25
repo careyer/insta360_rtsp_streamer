3ef7031f18f252d1c9a21fb1970a87411101aa62
careyer <tomyb2@gmx.net>
2019-07-24 21:07:23 -0400

    changes

--- trace_headers.sh
+++ trace_headers.sh
@@ -1,2 +1,4 @@
-ffmpeg -i testsrc.mp4 -c copy -bsf:v trace_headers -f null - 2>NALUS.txt
+#ffmpeg -i testsrc.mp4 -c copy -bsf:v trace_headers -f null - 2>NALUS.txt
+ffmpeg -i testsrc.mp4 -c copy -bsf:v trace_headers -f null -
+
 
