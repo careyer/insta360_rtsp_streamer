d7eb9979dd120f459d69b06083f70e1328dcb207
careyer <tomyb2@gmx.net>
2019-07-25 11:50:22 +0100

    insta360 proof of concept

--- gen_test_src.sh
+++ gen_test_src.sh
@@ -1,3 +1,3 @@
 #ffmpeg -re -f lavfi -i testsrc=duration=60:size=1280x720:rate=30 -f rtsp -muxdelay 0.1 -rtsp_transport udp rtsp://localhost:5545/test
 #ffmpeg -re -f lavfi -i testsrc=duration=60:size=1280x720:rate=30 -f rtsp -c:v libx264 -g 3 -muxdelay 0.1 -rtsp_transport udp rtsp://localhost:5545/test
-ffmpeg -re -f lavfi -i testsrc=duration=600:size=1280x720:rate=30 -vf format=yuv420p -f rtsp -c:v libx264 -profile:v baseline -level 3.0 -preset fast -g 3 -muxdelay 0.1 rtsp://localhost:5545/test
+ffmpeg -re -f lavfi -i testsrc=duration=60:size=1280x720:rate=30 -vf format=yuv420p -f rtsp -c:v libx264 -profile:v baseline -level 3.0 -preset fast -g 3 -muxdelay 0.1 rtsp://localhost:5545/test
