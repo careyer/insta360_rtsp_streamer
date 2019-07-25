a8a5fbffee70e3e18e1dea85220f3127776ad132
careyer <tomyb2@gmx.net>
2019-07-25 14:17:22 +0100

    tested trace_headers.sh

--- trace_headers.sh
+++ trace_headers.sh
@@ -1,4 +1,4 @@
-#ffmpeg -i testsrc.mp4 -c copy -bsf:v trace_headers -f null - 2>NALUS.txt
-ffmpeg -i testsrc.mp4 -c copy -bsf:v trace_headers -f null -
+ffmpeg -i testsrc.mp4 -c copy -bsf:v trace_headers -f null - 2>NALUS.txt
+#ffmpeg -i testsrc.mp4 -c copy -bsf:v trace_headers -f null -
 
 
